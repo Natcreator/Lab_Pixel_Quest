f3e6ba83fdb95fd857f2e31a5cbfa3ed3a375f71
Natcreator <nataliafabian24716@gmail.com>
2025-07-17 12:10:40 -0400

    pixel

--- Assets/Hands-On Homework #2/Scripts/HW2PlayerMovement.cs
+++ Assets/Hands-On Homework #2/Scripts/HW2PlayerMovement.cs
@@ -2,5 +2,19 @@ using UnityEngine;
 
 public class HW2PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D Rigidbody2D;
+
+
+
+    private void Start()
+    {
+       
+
+Rigidbody2D = GetComponent<Rigidbody2D>();
+
+
+    }
+
+
 
 }
---
+++ Assets/Pixel_Quest/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+
+{
+    private Rigidbody2D _rigidbody2D;
+    private SpriteRenderer sr1;
+    public int speed = 4;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        sr1 = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+
+        _rigidbody2D.velocity = new Vector2(xInput * speed, _rigidbody2D.velocity.y);
+
+        if (xInput == -1)
+        {
+            sr1.flipX = true;
+
+        }
+        else if (xInput == 0)
+        {
+
+            sr1.flipX = false;
+
+        }
+
+       
+                
+
+
+
+
+    }
+
+
+}
